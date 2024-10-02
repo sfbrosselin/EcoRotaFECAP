fbb990f1c610d19ad4bb51de9fc50d905308c0aa
Joao Pedro Brosselin <brosselindev@gmail.com>
2024-10-01 22:20:03 -0300

    mudança app.jsx
    adcionando rota pra pagina login e cadastro

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
 import Profile from "./pages/Profile"
+import Login from "./pages/Login"
+import SignUp from "./pages/SignUp"
 
 function App(){
   return (
@@ -8,6 +10,8 @@ function App(){
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/profile" element={<Profile/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/signup" element={<SignUp/>}/>
       </Routes>
     </Router>
     
